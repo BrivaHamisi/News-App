6d2f1ac6ec030078dc1e68f707df2ba810ef886d
BrivaHamisi <brivahamisi@gmail.com>
2024-02-25 19:36:46 +0300

    Search Bar

--- app/src/main/java/com/loc/newsapp/presentation/common/SearchBar.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/SearchBar.kt
@@ -74,6 +74,7 @@ fun Modifier.searchBar(): Modifier = composed {
     }
 }
 
+
 @Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
